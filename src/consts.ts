014c31f8b744a61500f733b90cbfd97990f11a90
elliottmckee <emckee3779@gmail.com>
2024-08-13 00:18:57 -0700

    initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Elliott McKee's Project Rambling Repo";
+export const SITE_DESCRIPTION = 'Blog to ramble about projects that manage to make it through to some degree of fruition';
