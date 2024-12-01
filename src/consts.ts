5291193c70c9453629197dcc66a777419bd1ff17
elliottmckee <emckee3779@gmail.com>
2024-12-01 13:50:12 -0800

    throwing out commit before flight - things are fucked

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Elliott McKee's Project Rambling Repo";
-export const SITE_DESCRIPTION = 'Blog to ramble about projects that manage to make it through to some degree of fruition';
+export const SITE_TITLE = "Elliott: Thoughts";
+export const SITE_DESCRIPTION = 'Space to ramble/pontificate on projects, thoughts, etc. that manage to make it through to some degree of fruition';
